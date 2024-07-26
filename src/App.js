38a5fc539de0088668652becb61213dc8ab0abf0
Sawera Tahir <141661220+SaweraTahir3@users.noreply.github.com>
2024-07-26 21:02:09 +0500

    sawera

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 
-// import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Question from './Components/Questions/QuestionPage';
-import  OutlinedCard from './Components/StudentsText/Student'
-import FreeSoloCreateOption from './Components/select/selectq'
+
+// import FirstPage from './Components/FirstPage/FirstPage';
+ import  AdminPage from './Components/AdminPage/AdminPage'
 
 function App() {
   return (
    
    <>
-  
-    <Navbar/>
-   <OutlinedCard/>
-    <Question/>
-    <FreeSoloCreateOption/>
+    {/* <FirstPage/> */}
+    <AdminPage/>
    </>
   );
 }
