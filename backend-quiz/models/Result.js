7004c5f28654abf4ec9e2264602f89b8bb497107
Sawera Tahir <141661220+SaweraTahir3@users.noreply.github.com>
2024-08-10 02:00:19 +0500

    pushing frontend code

--- backend-quiz/models/Result.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const resultSchema = new mongoose.Schema({
-  userId: String,
-  score: Number,
-  totalQuestions: Number
-});
-
-module.exports = mongoose.model('Result', resultSchema);
